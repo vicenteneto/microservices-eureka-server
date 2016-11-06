8ec8b2e409c277bf2529562728dd47abf61ad2af
Vicente Neto <sneto.vicente@gmail.com>
2016-11-06 12:21:51 -0300

    Adding .microservices to groupId

--- src/main/java/br/com/vneto/microservices/EurekaServerApplication.java
+++ src/main/java/br/com/vneto/microservices/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package br.com.vneto;
+package br.com.vneto.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/vneto/microservices/EurekaServerApplicationTests.java
+++ src/test/java/br/com/vneto/microservices/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.vneto;
+package br.com.vneto.microservices;
 
 import static org.junit.Assert.assertEquals;
 
